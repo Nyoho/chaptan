d2a1580a7a937ea36c0b99e1a010b1b47df0ad4e
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-10-24 20:12:07 +0900

    Fix rubocop

--- spec/chaptan_spec.rb
+++ spec/chaptan_spec.rb
@@ -14,11 +14,9 @@ RSpec.describe Chaptan do
   end
 end
 
-# TODO:
-# Add a test that the following successfully adds chapters
+# TODO: Add a test that the following successfully adds chapters
 # $ cp resources/example.mp3 /tmp/some_unique_directory_name
 # $ bundle exec exe/chaptan -y resources/chapters.yml /tmp/some_unique_directory_name/example.mp3
 
-# TODO:
-# Add a test that it successfully shows chapters
+# TODO: Add a test that it successfully shows chapters
 # $ bundle exec exe/chaptan resources/example.mp3
